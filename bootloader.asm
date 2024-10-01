ef584e3b3d70b251d78f8c8d30ef24edbca779f6
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-01 00:38:19 -0400

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -2,10 +2,10 @@ BITS 16
 ORG 0x7C00
 
 start:
-    mov ax, 0x0013  ; Set video mode to 320x200 256-color (Mode 13h)
+    mov ax, 0x0013 ; Set video mode to 320x200 256-color (Mode 13h)
     int 0x10
 
-
+    ; Load the kernel
     mov ax, 0x7E00    ; Load kernel from 0x7E00
     mov ds, ax
     mov si, 0         ; Offset in the kernel
